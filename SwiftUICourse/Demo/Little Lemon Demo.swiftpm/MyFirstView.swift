d2d4bcce40c44d2b112228f6206574e340b8822b
alireza <alireza.alemi76@icloud.com>
2023-11-30 16:14:00 -0700

    added first SwiftUI view.

---
+++ SwiftUICourse/Demo/Little Lemon Demo.swiftpm/MyFirstView.swift
@@ -0,0 +1,18 @@
+//
+//  MyFirstView.swift
+//  Little Lemon Demo
+//
+//  Created by AliReza on 2023-11-30.
+//
+
+import SwiftUI
+
+struct MyFirstView: View {
+    var body: some View {
+        Text("Little Lemon")
+    }
+}
+
+#Preview {
+    MyFirstView()
+}
